08981bc68fbe1a242925c0b577affee6270cda9f
Vaibhav2007Chaturvedi <vaibhav0090v@gmail.com>
2026-03-31 21:51:37 +0530

    python basics

---
+++ code.py
@@ -0,0 +1,8 @@
+# print("hello world")
+# print("vaibhav chaturvedi")
+#print("hello \n world","with python")
+#print("hello \nworld","with python")
+
+#VARIABLES:
+name="vaibhav"
+print("my name is: ",name)
