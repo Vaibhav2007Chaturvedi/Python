46f54692288a693845afb3762d8f34aa10f7b039
Vaibhav2007Chaturvedi <vaibhav0090v@gmail.com>
2026-04-05 10:42:40 +0530

    Range function

---
+++ range.py
@@ -0,0 +1,17 @@
+# RANGE() FUNNCTION:
+
+# x=range(10)
+# for n in x:
+#     print(n)
+
+# x=range(3,6)
+# for n in x:
+#     print(n)
+
+# x=range(0,30,5)
+# for n in x:
+#     print(n)
+
+x=range(0,-9,-1)
+for n in x:
+    print(n)
