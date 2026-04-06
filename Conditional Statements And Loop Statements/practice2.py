f1b5d27bdbc1623fe1d1fba59378ff7662e676ad
Vaibhav2007Chaturvedi <vaibhav0090v@gmail.com>
2026-04-06 11:52:40 +0530

    CONDITIONAL AND LOOP STATEMENT QUESTION PART2

---
+++ Conditional Statements And Loop Statements/practice2.py
@@ -0,0 +1,38 @@
+# Write A Program to accept a character from the user and display whether it is a vowel or consonant:
+# ch=input("Enter a character: ")
+# if (ch=='a' or ch=='i' or ch=='e' or ch=='o' or ch=='u'):
+#     print("It is a vowel: ",ch)
+# elif (ch=='A' or ch=='I' or ch=='E' or ch=='O' or ch=='U'):
+#     print("It is a vowel: ",ch)
+# else:
+#     print("It is a consonant")
+
+# ............................................................
+
+# Write a Program Fibonacci Series up to a certain limit:
+# i=int(input("Enter a Number: "))
+# x=0
+# y=1
+# z=1
+# print(x,y,end=" ")
+# while(z<=i):
+#     print(z,end=' ')
+#     x=y
+#     y=z
+#     z=x+y
+
+# ....................................................
+
+# Write A program to enter a number and check if it is a prime number or not:
+# num = int(input('Enter the number: '))
+# if num > 1:
+#     for i in range(2, num // 2 + 1):
+#         if num % i == 0:
+#             print('It is not a prime number')
+#             break
+#     else:
+#         print('It is a prime number')
+# else:
+#     print('It is not a prime number')
+
+# .........................................................
